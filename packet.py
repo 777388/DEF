3c62cff0d91238c7a9013682407a16efa5c3972c
777388 <96343159+777388@users.noreply.github.com>
2023-04-06 15:01:43 -0700

    Update packet.py

--- packet.py
+++ packet.py
@@ -1,18 +1,24 @@
+import base64
 from scapy.all import *
 
-# Define a function to modify packets
-def modify_packet(packet):
-    # Modify the packet as needed
+# Define a function to modify and send packets
+def modify_and_send_packet(packet):
+    # Base64 encode the payload of the packet
     packet[Raw].load += b" <a href='https://www.youtube.com/watch?v=2QF0ylMF0pA'>Click here</a>"  # Append a weblink to the end of the packet
+    payload = packet[Raw].load
+    encoded_payload = base64.b64encode(payload)
 
-    # Return the modified packet
-    return packet
+    # Replace the payload with the base64 encoded payload
+    packet[Raw].load = encoded_payload
+
+    # Send the modified packet out on the network
+    send(packet)
 
 # Set up a packet capture filter to capture outgoing packets
 capture_filter = "dst host not localhost and outbound"
 
 # Set up a packet capture object to capture outgoing packets
-capture = sniff(filter=capture_filter, prn=modify_packet)
+capture = sniff(filter=capture_filter, prn=modify_and_send_packet)
 
 # Start the packet capture
 capture.run()
